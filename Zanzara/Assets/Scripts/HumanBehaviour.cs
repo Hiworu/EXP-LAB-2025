63a1a772ec739fbbe1c4ecb88b246a28016789b1
Elia Folisi <eliafolisi@gmail.com>
2025-02-16 14:06:41 +0100

    merda che ho dimenticato

--- Zanzara/Assets/Scripts/HumanBehaviour.cs
+++ Zanzara/Assets/Scripts/HumanBehaviour.cs
@@ -2,30 +2,33 @@ using UnityEngine;
 
 public class HumanBehaviour : MonoBehaviour
 {
-
     public GameObject head;
     public GameObject viewCone;
 
     public GameObject player;
     private GameObject cones;
+    
     public bool isColliding = false;
 
     private float nextRotationTime = 0f;
     private float rotationInterval = 2f; // Adjust the interval as needed
     private bool isRotating = false;
+    private bool headUpDown = false;
     private float rotationSpeed = 2f; // Adjust the speed as needed
     private Quaternion targetRotation;
+    private float coneRotationSpeed = 30f; // Speed of the cone rotation
 
-    private void Start()
+
+     private void Start()
     {
         player = GameObject.Find("Player");
         cones = GameObject.Find("ViewCones");
-        cones.SetActive(false);
+
     }
 
     private void Update()
     {
-        if(isRotating == true)
+        if (isRotating == true)
         {
             if (Time.time >= nextRotationTime)
             {
@@ -37,13 +40,17 @@ public class HumanBehaviour : MonoBehaviour
             // Smoothly rotate towards the target rotation
             head.transform.localRotation = Quaternion.Lerp(head.transform.localRotation, targetRotation, Time.deltaTime * rotationSpeed);
         }
-        
     }
 
     private void RotateHeadRandomly()
     {
-        float randomYRotation = Random.Range(-90f, 90f); // 180 degrees range
+        float randomYRotation = Random.Range(0f, 180); // 180 degrees range
+        float randomZRotation = Random.Range(-30f, 30); 
         targetRotation = Quaternion.Euler(0, randomYRotation, 0);
+        if(headUpDown == true)
+        {
+            targetRotation = Quaternion.Euler(0, 0, randomZRotation);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -64,17 +71,17 @@ public class HumanBehaviour : MonoBehaviour
                     Debug.Log(gameObject.name);
                     cones.SetActive(true);
                     isRotating = true;
-                    //head movement + periferal cone view che si attiva e disattiva
+                    //head movement + periferal cone view 
                     break;
                 case "Awareness2":
                     Debug.Log(gameObject.name);
                     cones.SetActive(true);
-                    cones.transform.localScale = new Vector3(2, 2, 2);
+                    headUpDown = true;
+                    //cones.transform.localScale = new Vector3(2, 2, 2);
                     isRotating = true;
-                    //head movement + bigger view cone always active + player death if inside
+                    //head movement + bigger view cone moves up and down + player death if inside
                     break;
             }
         }
-        
     }
 }
